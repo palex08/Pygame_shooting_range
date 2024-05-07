154d94fbd70dcff76eda1c72ef9fe657fd27455d
Alexander <82374540+palex08@users.noreply.github.com>
2024-05-07 15:06:23 +1000

    add part_cycle-code

--- main.py
+++ main.py
@@ -17,12 +17,33 @@ pygame.display.set_icon(icon)
 color_screen = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 target_img = pygame.image.load("img/target.png")
-target_img = pygame.transform.scale(target_img, (50, 100))
-target_x = random.randint(0, SCREEN_WIDTH - 50)
-target_y = random.randint(0, SCREEN_HEIGHT - 100)
+target_img = pygame.transform.scale(target_img, (120, 150))
+target_x = random.randint(0, SCREEN_WIDTH - 120)
+target_y = random.randint(0, SCREEN_HEIGHT - 150)
+
+cursor_img = pygame.image.load("img/cursor.png")
+pygame.mouse.set_visible(False)
+
 
 running = True
 while running:
-    pass
+    screen.fill(color_screen)
+    screen.blit(target_img, (target_x, target_y))
+    mouse_pos = pygame.mouse.get_pos()
+    cursor_pos = (mouse_pos[0] - cursor_img.get_width() / 2, mouse_pos[1] - cursor_img.get_height() / 2)
+    screen.blit(cursor_img, cursor_pos)
+    pygame.display.update()
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+            if target_x + 33 < event.pos[0] < target_x + 87 and target_y + 45 < event.pos[1] < target_y + 115:
+                target_x = random.randint(0, SCREEN_WIDTH - 120)
+                target_y = random.randint(0, SCREEN_HEIGHT - 150)
+            elif target_x + 50 < event.pos[0] < target_x + 73 and target_y + 5 < event.pos[1] < target_y + 37:
+                target_x = random.randint(0, SCREEN_WIDTH - 120)
+                target_y = random.randint(0, SCREEN_HEIGHT - 150)
 pygame.quit()
